92bc022cd3130dd8f0eaddafd3b78c844ea5936b
tvc141003 <iamtvcitlk@gmail.com>
2023-03-23 22:36:57 +0700

    Debug add file name when create file

--- nachos/nachos/nachos-3.4/code/test/createfile.c
+++ nachos/nachos/nachos-3.4/code/test/createfile.c
@@ -6,18 +6,21 @@ int main()
 	int len;
 	char filename[maxlen +1];
 /*Create a file*/
-	if (Create("texTTTT1.txt") == -1)
+	PrintString("Input file name: ");
+	ReadString(filename, maxlen);
+
+	if (Create(filename) == -1)
 	{
 	// xuất thông báo lỗi tạo tập tin
 	//printf("xuất thông báo lỗi tạo tập tin");
 
-		PrintString("hsdnlaknsdlaknd");
+		PrintString("Unsuccess!");
 	}
 	else
 	{
 	// xuất thông báo tạo tập tin thành công
 	//printf("xuất thông báo tạo tập tin thành công");
-		PrintString("hsdnlaknsdlakndqwqwq");
+		PrintString("Success!");
 	}
 
 	Halt();
